71317cc823e77679863fd059098411fc1bee431e
Kaifeng Gao <kaifeng.gao@outlook.com>
2024-11-17 14:57:36 -0500

    implement earlystopper for training

--- notes.md
+++ notes.md
@@ -4,8 +4,16 @@
 - Implement Neighbor Sampler to prevent lable leakage in training (though not a big influence)
 - Modulize the code and run more experiments
 - Parallelly run a graphformer if possible
+- Add documentation for LAGAT Conv Class
 
 ## Done
 - Improve LAGAT Conv Layer for efficiency
-    - precompute alpha value instead of concatenation in message
-    - use torch operation to 
+    - Precompute alpha value instead of concatenation in message
+    - Use torch operation to speed up indexing
+- Fix bugs caused by "hint comment" (used by TorchScript JIT to infer the propagate type):
+- Implement LAGAT on top of current GATConv from PyG
+    - Add label attention mechanism to current GAT
+
+
+
+- 
---
+++ utils/earlystopping.py
@@ -0,0 +1,52 @@
+import os
+import numpy as np
+import torch
+
+class EarlyStopping:
+    def __init__(self, dataset_name, timestamp, patience=30, log_dir=None):
+        self._filename = f"early_stop_{timestamp}.pth"
+        self._save_dir = os.path.join('checkpoints', dataset_name)
+        if log_dir:
+            self._save_dir = os.path.join(log_dir, self._save_dir)
+
+        os.makedirs(self._save_dir, exist_ok=True)
+        self.save_path = os.path.join(self._save_dir, self._filename)
+        print(f"[{self.__class__.__name__}]: Saving model to {self.save_path}")
+
+        self.patience = patience
+        self.counter = 0
+        self.best_epoch = -1
+        self.best_loss = None
+        self.best_result = None
+        self.early_stop = False
+
+    def step(self, epoch, loss, result, model):
+        if self.best_loss is None:
+            self.best_epoch = epoch
+            self.best_result = result
+            self.best_loss = loss
+            self.save_checkpoint(model)
+        elif result < self.best_result:
+            self.counter += 1
+            # print(f'EarlyStopping counter: {self.counter} out of {self.patience} in epoch {epoch}\n')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            if result >= self.best_result:
+                self.save_checkpoint(model)
+            self.best_epoch = epoch
+            self.best_loss = np.min((loss, self.best_loss))
+            self.best_result = np.max((result, self.best_result))
+            self.counter = 0
+
+        return self.early_stop
+    
+    def save_checkpoint(self, model):
+        """Saves model when validation loss decreases."""
+        # print("Model saved to: ", self.save_path)
+        torch.save(model.state_dict(), self.save_path)
+
+    def load_checkpoint(self, model):
+        """Load the latest checkpoint."""
+        model.load_state_dict(torch.load(self.save_path))
+
